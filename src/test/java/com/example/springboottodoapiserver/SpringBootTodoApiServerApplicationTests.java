bb13c958b767a8640a81c2f3e1a11a1e068b0aa6
Erik Yuzwa <erikyuzwa@gmail.com>
2022-06-18 10:40:29 -0600

    initial commit

---
+++ src/test/java/com/example/springboottodoapiserver/SpringBootTodoApiServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springboottodoapiserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootTodoApiServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
