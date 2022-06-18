4ca3bd2e07ec92a41f2819d6b81dd673e767f27f
Erik Yuzwa <erikyuzwa@gmail.com>
2022-06-18 15:10:37 -0600

    flesh out CRUD mappings

--- src/main/java/com/example/springboottodoapiserver/controllers/TodoItemController.java
+++ src/main/java/com/example/springboottodoapiserver/controllers/TodoItemController.java
@@ -2,17 +2,20 @@ package com.example.springboottodoapiserver.controllers;
 
 import com.example.springboottodoapiserver.models.TodoItem;
 import com.example.springboottodoapiserver.repositories.TodoItemRepository;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.*;
 
+import javax.validation.Valid;
+import java.time.Instant;
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/api/v1/todoitems")
 public class TodoItemController {
 
-    private TodoItemRepository todoItemRepository;
+    private final TodoItemRepository todoItemRepository;
 
     public TodoItemController(TodoItemRepository todoItemRepository) {
         this.todoItemRepository = todoItemRepository;
@@ -22,4 +25,47 @@ public class TodoItemController {
     public List<TodoItem> findAllTodoItems() {
         return (List<TodoItem>) todoItemRepository.findAll();
     }
+
+    @PostMapping("")
+    public TodoItem createTodoItem(@RequestBody TodoItem todoItem) {
+
+        TodoItem newItem = new TodoItem();
+        newItem.setDescription(todoItem.getDescription());
+        newItem.setComplete(todoItem.isComplete());
+        newItem.setCreatedDate(Instant.now());
+        newItem.setModifiedDate(Instant.now());
+        newItem = todoItemRepository.save(newItem);
+        return newItem;
+    }
+
+    @GetMapping("/{id}")
+    public Optional<TodoItem> getTodoItem(@PathVariable("id") long id) {
+        return todoItemRepository.findById(id);
+    }
+
+    @PutMapping("/{id}")
+    public TodoItem updateTodoItem(@PathVariable("id") long id, @RequestBody TodoItem todoItem) {
+
+        Optional<TodoItem> item = todoItemRepository.findById(id);
+        TodoItem updatedItem = new TodoItem();
+        if (item.isPresent()) {
+            updatedItem = item.get();
+            updatedItem.setDescription(todoItem.getDescription());
+            updatedItem.setComplete(todoItem.isComplete());
+            updatedItem.setModifiedDate(Instant.now());
+            updatedItem = todoItemRepository.save(updatedItem);
+        }
+
+        return updatedItem;
+    }
+
+    @DeleteMapping("/{id}")
+    public void deleteTodoItem(@PathVariable("id") long id) {
+        // todoItemRepository.deleteById(id);
+        Optional<TodoItem> item = todoItemRepository.findById(id);
+        if (item.isPresent()){
+            todoItemRepository.delete(item.get());
+        }
+    }
+
 }
